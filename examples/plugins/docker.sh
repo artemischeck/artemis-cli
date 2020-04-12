f9861438c74be2eeaebd08da2206821bb0f5f830
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2020-04-12 21:07:52 +0300

    Added docker monitor

--- examples/plugins/docker.sh
+++ examples/plugins/docker.sh
@@ -1,5 +1,4 @@
-if [ $(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "true" ]; then
-    # echo yup
-else
+INSPECT=$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)
+if [ "$INSPECT" -ne true ]; then
     echo "$CONTAINER_NAME is down"
 fi
